c6931ebb00be6e5bde972e6ef15246e7b8ac6789
jsonLiang <yuanyu90221@gmail.com>
2020-07-23 00:31:22 +0800

    set up with pg library but recommand pgx

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
 	"regexp"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
 )
 
 func normalize(phone string) string {
@@ -9,15 +15,26 @@ func normalize(phone string) string {
 	return re.ReplaceAllString(phone, "")
 }
 
-// func normalize(phone string) string {
-// 	var buf bytes.Buffer
-// 	for _, ch := range phone {
-// 		if ch >= '0' && ch <= '9' {
-// 			buf.WriteRune(ch)
-// 		}
-// 	}
-// 	return buf.String()
-// }
 func main() {
+	host := os.Getenv("HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	passwd := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable", host, port, user, passwd)
+	// db, err := sql.Open("postgres", psqlInfo)
+	// must(err)
+	// db.Close()
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
 
+	must(db.Ping())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
